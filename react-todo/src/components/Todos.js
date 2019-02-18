d46423918e79b73bb3cb52085023ef0263a435a1
enomotor <rene.enomoto@asia-quest.jp>
2019-02-18 10:01:51 +0900

    files added

---
+++ react-todo/src/components/Todos.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import TodoItem from './TodoItem';
+import PropTypes from 'prop-types';
+
+class Todos extends Component {
+   
+
+
+  render() {
+      console.log(this.props.todos)
+    return this.props.todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} 
+        markComplete={this.props.markComplete} 
+        delTodo={this.props.delTodo}/>
+    ));
+  }
+}
+
+// PropTypes
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTpdo: PropTypes.func.isRequired,
+}
+
+export default Todos;
+
+
+
